ac330a52595c3894fe251f9848158a289113c72f
Daniel Money <daniel_money@yahoo.co.uk>
2019-06-17 01:57:07 +0100

    Add class to calculate similarity on the fly (with cache)

--- src/Similarity/CalculatedSimilar.java
+++ src/Similarity/CalculatedSimilar.java
@@ -1,5 +1,33 @@
 package Similarity;
 
-public class CalculatedSimilar
+import Correlation.Correlation;
+
+public class CalculatedSimilar implements Similar
 {
+    public CalculatedSimilar(Correlation corr, byte[][] data, int n)
+    {
+        this.corr = corr;
+    }
+
+    @Override
+    public Integer[] getSimilar(int p)
+    {
+        if (p == cachedP)
+        {
+            return cached;
+        }
+        else
+        {
+            Integer[] a = new Integer[n];
+            a = corr.topn(data,n,p).toArray(a);
+            return a;
+        }
+    }
+
+    private Correlation corr;
+    private byte[][] data;
+    private int n;
+
+    private int cachedP;
+    private Integer[] cached;
 }
